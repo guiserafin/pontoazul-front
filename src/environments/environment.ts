5a8ebdc6baa9bd93b1ed4d4dfbf496802dfd49ec
Guilherme Serafin de Carvalho <98965845+guiserafin@users.noreply.github.com>
2025-11-25 11:13:13 -0300

    Update production API endpoint in environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.pontoazul.com' // TODO: Update with the production API endpoint.
+  apiUrl: 'https://pontoazul-production.up.railway.app'
 };
